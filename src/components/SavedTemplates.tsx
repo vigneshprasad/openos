acac6ae2dbc223d9e63970edcbdf9532064ce107
sanjeev40 <sanjeevraichur29@gmail.com>
2023-05-09 03:07:20 +0530

    Update saved templates

--- src/components/SavedTemplates.tsx
+++ src/components/SavedTemplates.tsx
@@ -1,6 +1,23 @@
 import { InfoCircledIcon } from "@radix-ui/react-icons"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useCallback } from "react"
+import { SAVED_TEMPLATES } from "~/constants/reportConstants"
 
 export const SavedTemplates: React.FC = () => {
+    const router = useRouter();
+
+    const executeTemplate = useCallback(async (command: string) => {
+        if (command && router) {
+            await router.push({
+                pathname: "/",
+                query: {
+                    exec: command
+                }
+            })
+        }
+    }, [router])
+
     return (
         <div>
             <div className="pb-[18px] flex gap-1 items-center">
@@ -10,36 +27,45 @@ export const SavedTemplates: React.FC = () => {
                 </p>
             </div>
 
-            <div className="pb-6 flex flex-col gap-4">
-                {Array(4).fill("").map((item, index) => (
-                    <div className="h-max" key={index}>
-                        <div className="min-h-[108px] pt-3 pb-4 px-4 bg-[#1C1C1C] rounded-md grid grid-cols-[1fr_max-content] 
-                            gap-3 items-start">
-                            <div>
-                                <h1 className="text-sm font-semibold text-[#fff]">
-                                    Cash Burn
-                                </h1>
-                                <p className="pt-1 pb-3 text-sm font-medium text-[#616161]">
-                                    See how much you spend every month and see where your cash goes
-                                </p>
-                                <div className="flex gap-2">
-                                    <div className="py-0.5 px-2 bg-[#2B2B2B] rounded-[4px] text-xs 
-                                        font-medium text-[#C4C4C4]">
-                                        Report
+            <div className="pb-8 flex flex-col gap-4">
+                {SAVED_TEMPLATES.map((template) => (
+                    template.status && (
+                        <div className="h-max" key={template.key}>
+                            <div className="min-h-[108px] pt-3 pb-4 px-4 bg-[#1C1C1C] rounded-md grid grid-cols-[1fr_max-content] 
+                                gap-3 items-start">
+                                <div>
+                                    <h1 className="text-sm font-semibold text-[#fff]">
+                                        {template.title}
+                                    </h1>
+                                    <p className="pt-1 pb-3 text-sm font-medium text-[#616161]">
+                                        {template.description}
+                                    </p>
+                                    <div className="flex gap-2">
+                                        {template.tags.map((tag, index) => (
+                                            <div className="py-0.5 px-2 bg-[#2B2B2B] rounded-[4px] text-xs 
+                                                font-medium text-[#C4C4C4]" key={index}>
+                                                {tag}
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
-                            </div>
-                            <div className="flex gap-5 items-center">
-                                <p className="text-sm font-normal text-[#5EA3FB] cursor-pointer hover:underline">
-                                    Preview
-                                </p>
-                                <button className="py-[7.5px] px-3 bg-[#333134] rounded-md max-h-9 text-sm font-normal 
-                                    text-[#fff] cursor-pointer hover:bg-[#373737]">
-                                    Use Template
-                                </button>
+                                <div className="flex gap-5 items-center">
+                                    <Link href={template.preview} target="_blank">
+                                        <p className="text-sm font-normal text-[#5EA3FB] cursor-pointer hover:underline">
+                                            Preview
+                                        </p>
+                                    </Link>
+                                    <button 
+                                        className="py-[7.5px] px-3 bg-[#333134] rounded-md max-h-9 text-sm font-normal 
+                                            text-[#fff] cursor-pointer hover:bg-[#373737]"
+                                        onClick={() => executeTemplate(template.command)}
+                                    >
+                                        Use Template
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )
                 ))}
             </div>
         </div>
