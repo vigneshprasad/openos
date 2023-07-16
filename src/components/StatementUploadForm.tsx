30d941aa3f0712f11b0532960d135111079ff74e
Vignesh Prasad <vigfb9295@gmail.com>
2023-07-16 23:33:10 +0530

    integrations page styling

--- src/components/StatementUploadForm.tsx
+++ src/components/StatementUploadForm.tsx
@@ -6,6 +6,7 @@ import { CrossCircledIcon } from '@radix-ui/react-icons';
 import Image from "next/image";
 import useAnalytics from "~/utils/analytics/AnalyticsContext";
 import { AnalyticsEvents } from "~/utils/analytics/types";
+import { PrimaryButton } from "./PrimaryButton";
 
 export const StatementUploadForm: React.FC = () => {    
     const [name, setName] = useState<string>("");
@@ -98,18 +99,24 @@ export const StatementUploadForm: React.FC = () => {
         <Dialog.Root open={open} onOpenChange={setOpen}>
             <Dialog.Trigger asChild>
                 {success ? 
-                    <button className="Button bg-[#262626] mt-1 h-9 flex text-[#49C179]" disabled>
-                        <Image src="/svg/green_tick.svg" alt="Green Tick" width={16} height={16} />
+                    <button className="bg-primary/10 text-[#49C179] rounded-md mt-3 py-2 px-3
+                    font-normal text-xs flex gap-1.5" disabled>
+                        <Image src="/svg/green_tick.svg" alt="Green Tick" width={15} height={15} />
                         Connected
                     </button> :
-                    <button className="Button secondary mt-1 h-9">Connect</button>
+                    <button className="bg-primary/10 text-primary rounded-md mt-3 py-2 px-3
+                    font-normal text-xs flex gap-1.5
+                    hover:bg-primary/20 cursor-pointer"
+                    >
+                        Connect
+                    </button>
                 }
             </Dialog.Trigger>
             <Dialog.Portal>
                 <Dialog.Overlay className="DialogOverlay" />
                 <Dialog.Content className="DialogContent p-4">
-                    <div className="pb-4 flex flex-row justify-between items-center">
-                        <text className="text-md text-[#fff]">Add Bank Statement</text>
+                    <div className="pb-8 flex flex-row justify-between items-center">
+                        <text className="text-md text-[#242533]">Add Bank Statement</text>
                         <Dialog.Close asChild>
                             <CrossCircledIcon color="#C4C4C4" className="cursor-pointer" />
                         </Dialog.Close>
@@ -117,9 +124,9 @@ export const StatementUploadForm: React.FC = () => {
                     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                         <fieldset className="Fieldset">
                             <label className="Label">
-                                <span>Name of Resource</span>
+                                <span className="text-[#242533]">Name of Resource</span>
                                 <input
-                                    className="Input" 
+                                    className="bg-slate-50 rounded-lg p-2 pl-3 w-full"
                                     type="text"
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
@@ -128,16 +135,16 @@ export const StatementUploadForm: React.FC = () => {
                         </fieldset>
                         <input 
                             type="file" 
-                            className="block w-full text-sm text-[#C4C4C4] file:mr-6 
-                            file:py-2 file:px-2 file:rounded-full file:border-0 file:text-sm file:bg-[#373737]
-                            file:text-[#fff] hover:file:bg-[#202020] file:cursor-pointer"
+                            className="block w-full text-[#242533] file:mr-6 
+                            file:py-2 file:px-2 text-xs file:border-0 file:rounded-md file:bg-primary/10
+                            file:text-primary hover:file:bg-primary/20 file:cursor-pointer"
                             onChange={handleFileUpload}
                         />
-                        <div className="pt-2 flex justify-center">
+                        <div className="flex justify-center">
                             {loading ?
-                                <button type="submit" disabled className="Button secondary">Loading...</button>
+                                <PrimaryButton type="submit" disabled className="mt-8">Loading...</PrimaryButton>
                                 :
-                                <button type="submit" className="Button primary">Connect</button>
+                                <PrimaryButton type="submit" className="mt-8">Connect</PrimaryButton>
                             }
                         </div>
                     </form>
