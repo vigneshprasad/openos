cb8be0d67a1147868bb97349281bf8fea0c8ff5b
Vignesh Prasad <vigfb9295@gmail.com>
2023-08-09 02:23:02 -0400

    minor dummy data fix

--- src/constants/fakerFunctions.ts
+++ src/constants/fakerFunctions.ts
@@ -23,8 +23,8 @@ export const getDummyIncludeAndExclude = (date: string, modelId: string, period:
         })
     }
     users = users.sort((a, b) => b.probability - a.probability)
-    const includeUsers = users.slice(0, 5)
-    const excludeUsers = users.slice(5, 10).reverse()
+    const excludeUsers = users.slice(0, 5)
+    const includeUsers = users.slice(5, 10).reverse()
 
     return {
         include: {
